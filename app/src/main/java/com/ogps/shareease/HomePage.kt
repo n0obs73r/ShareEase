844cdd2b57f4ce4e481fad4de87abf4e0769e9bd
Aryan Dadheech <aryan.prince999@gmail.com>
2021-11-17 21:18:02 +0530

    Icon updated Fragments Added

--- app/src/main/java/com/ogps/shareease/HomePage.kt
+++ app/src/main/java/com/ogps/shareease/HomePage.kt
@@ -2,10 +2,39 @@ package com.ogps.shareease
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.ogps.shareease.fragments.Profile
+import com.ogps.shareease.fragments.Publish
+import com.ogps.shareease.fragments.Search
 
 class HomePage : AppCompatActivity() {
+
+    private val profile = Profile()
+    private val publish = Publish()
+    private val search = Search()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+        replaceFragment(search)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.btmnav)
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_profile -> replaceFragment(profile)
+                R.id.ic_search -> replaceFragment(search)
+                R.id.ic_publish -> replaceFragment(publish)
+            }
+            true
+        }
+
     }
+
+    private fun replaceFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container,fragment)
+        transaction.commit()
+    }
+
 }
---
+++ app/src/main/java/com/ogps/shareease/fragments/Profile.kt
@@ -0,0 +1,29 @@
+package com.ogps.shareease.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.firebase.database.DatabaseReference
+import com.ogps.shareease.R
+
+
+class Profile : Fragment() {
+    private lateinit var database: DatabaseReference
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+
+}
