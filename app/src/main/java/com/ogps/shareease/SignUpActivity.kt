793c66ded106361e7809f212e63c988cbc732292
Aryan Dadheech <aryan.prince999@gmail.com>
2021-11-19 01:27:49 +0530

    Profile Picture Added

--- app/src/main/java/com/ogps/shareease/SignUpActivity.kt
+++ app/src/main/java/com/ogps/shareease/SignUpActivity.kt
@@ -1,31 +1,46 @@
 package com.ogps.shareease
 
+import android.R.attr
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import android.content.ContentValues.TAG
+import android.net.Uri
+import android.provider.MediaStore
+import android.widget.*
+import android.graphics.BitmapFactory
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import java.io.File
+import java.io.FileNotFoundException
+
 
 open class SignUpActivity : AppCompatActivity() {
 
+
     private lateinit var auth: FirebaseAuth
     private val db = Firebase.firestore
+    private val storageReference = FirebaseStorage.getInstance().getReference()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-
-
+        auth = FirebaseAuth.getInstance()
+        val pfpButton : Button = findViewById(R.id.pfp)
         val signUpButton : Button = findViewById(R.id.signup)
         signUpButton.setOnClickListener {
             signUp()
+        }
+        pfpButton.setOnClickListener {
+            updatepfp()
         }
 
         val loginButton : Button = findViewById(R.id.login_page_btn)
@@ -34,9 +49,49 @@ open class SignUpActivity : AppCompatActivity() {
         }
     }
 
+    private fun updatepfp() {
+
+        val Galleryintent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(Galleryintent,1000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val pfpView : ImageView = findViewById(R.id.displaypfp)
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 1000){
+            if (resultCode === RESULT_OK && data!= null) {
+                // Get the Uri of data
+                val filePath = data.data
+                try {
+                    val inputStream = contentResolver.openInputStream(filePath!!)
+                    val bitmap = BitmapFactory.decodeStream(inputStream)
+                    pfpView!!.setImageBitmap(bitmap)
+                    //imageStore(bitmap)
+                    uploadImageToFirebase(filePath)
+                } catch (e: FileNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+
+        }
+    }
+
+    private fun uploadImageToFirebase(imageUri: Uri) {
+        val storageRef = storageReference
+        storageRef.child("users/${auth.currentUser!!.uid}/profile_picture.jpg")
+        storageRef.putFile(imageUri).addOnSuccessListener(OnSuccessListener<UploadTask.TaskSnapshot>(){
+            Toast.makeText(this, "Image Uploaded!", Toast.LENGTH_SHORT).show()
+        }).addOnFailureListener(){
+            Toast.makeText(this, "Image Not Uploaded!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
     private fun signUp() {
         val profile = getDetails()
         val userId = profile["email"]
+
 
         if (userId != null) {
             db.collection("users").document(userId).set(profile).addOnSuccessListener {
@@ -46,7 +101,7 @@ open class SignUpActivity : AppCompatActivity() {
             }
 
             val password = findViewById<EditText>(R.id.signup_password).text.toString()
-            auth = FirebaseAuth.getInstance()
+
             auth.createUserWithEmailAndPassword(userId, password).addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     Toast.makeText(this, "Successfully Registered", Toast.LENGTH_LONG).show()
--- app/src/main/java/com/ogps/shareease/fragments/Profile.kt
+++ app/src/main/java/com/ogps/shareease/fragments/Profile.kt
@@ -1,20 +1,28 @@
 package com.ogps.shareease.fragments
 
 import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.ogps.shareease.HomePage
+import com.ogps.shareease.MainActivity
 import com.ogps.shareease.R
+import com.ogps.shareease.SignUpActivity
 
 private const val USER_ID = "UserID"
 
-class Profile : Fragment() {
+class Profile : Fragment()  {
 
     private val db = Firebase.firestore
 
@@ -36,7 +44,19 @@ class Profile : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         rootView =  inflater.inflate(R.layout.fragment_profile, container, false)
+        val logout : Button = rootView.findViewById(R.id.logout)
+        logout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            logout()
+        }
+
         return rootView
+
+    }
+
+    private fun logout(){
+        val intent = Intent(requireActivity().application, MainActivity::class.java)
+        startActivity(intent)
     }
 
     private fun getProfile() {
